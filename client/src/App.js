603aed2d9c7bd85e4dbf3f09b0ce8251eb75a341
Paulo Roberto Campos de Vasconcelos <paulorcv@gmail.com>
2022-05-24 16:22:09 -0300

    Deploy test

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Opa!!
+            Opa!!!!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
